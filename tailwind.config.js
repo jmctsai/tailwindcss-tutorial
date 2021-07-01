f896c7f536f8def28daf0585b465dc2dae997c32
Johnny Tsai <johnnytsai97@gmail.com>
2021-07-01 12:34:02 -0700

    completed TailwindCSS tutorial

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	purge: ['./src/**/*.html', './src/**/*.js'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+}
